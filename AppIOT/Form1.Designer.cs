2138d2d88ec9408e962ee3ea715006e203672a20
Miguel Silva <2221462@my.ipleiria.pt>
2024-11-29 17:03:42 +0000

    GET THIS SHIT OUT OF MEEEEEEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ AppIOT/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace AppIOT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxNomeSensor = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxAppName = new System.Windows.Forms.TextBox();
+            this.btnCreateApp = new System.Windows.Forms.Button();
+            this.btnCreateContainer = new System.Windows.Forms.Button();
+            this.txtBoxMosquitto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBoxNomeSensor
+            // 
+            this.txtBoxNomeSensor.Location = new System.Drawing.Point(117, 25);
+            this.txtBoxNomeSensor.Name = "txtBoxNomeSensor";
+            this.txtBoxNomeSensor.Size = new System.Drawing.Size(100, 22);
+            this.txtBoxNomeSensor.TabIndex = 0;
+            this.txtBoxNomeSensor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nome Luz:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nome App:";
+            // 
+            // txtBoxAppName
+            // 
+            this.txtBoxAppName.Location = new System.Drawing.Point(12, 25);
+            this.txtBoxAppName.Name = "txtBoxAppName";
+            this.txtBoxAppName.Size = new System.Drawing.Size(100, 22);
+            this.txtBoxAppName.TabIndex = 5;
+            // 
+            // btnCreateApp
+            // 
+            this.btnCreateApp.Location = new System.Drawing.Point(12, 53);
+            this.btnCreateApp.Name = "btnCreateApp";
+            this.btnCreateApp.Size = new System.Drawing.Size(100, 32);
+            this.btnCreateApp.TabIndex = 7;
+            this.btnCreateApp.Text = "Criar App";
+            this.btnCreateApp.UseVisualStyleBackColor = true;
+            this.btnCreateApp.Click += new System.EventHandler(this.btnCreateApp_Click);
+            // 
+            // btnCreateContainer
+            // 
+            this.btnCreateContainer.Enabled = false;
+            this.btnCreateContainer.Location = new System.Drawing.Point(117, 53);
+            this.btnCreateContainer.Name = "btnCreateContainer";
+            this.btnCreateContainer.Size = new System.Drawing.Size(100, 32);
+            this.btnCreateContainer.TabIndex = 8;
+            this.btnCreateContainer.Text = "Criar Luz";
+            this.btnCreateContainer.UseVisualStyleBackColor = true;
+            this.btnCreateContainer.Click += new System.EventHandler(this.btnCreateContainer_Click);
+            // 
+            // txtBoxMosquitto
+            // 
+            this.txtBoxMosquitto.Location = new System.Drawing.Point(12, 113);
+            this.txtBoxMosquitto.Multiline = true;
+            this.txtBoxMosquitto.Name = "txtBoxMosquitto";
+            this.txtBoxMosquitto.ReadOnly = true;
+            this.txtBoxMosquitto.Size = new System.Drawing.Size(205, 159);
+            this.txtBoxMosquitto.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "ReceivedValues";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 292);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtBoxMosquitto);
+            this.Controls.Add(this.btnCreateContainer);
+            this.Controls.Add(this.btnCreateApp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxAppName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxNomeSensor);
+            this.Name = "Form1";
+            this.Text = "Luz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxNomeSensor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxAppName;
+        private System.Windows.Forms.Button btnCreateApp;
+        private System.Windows.Forms.Button btnCreateContainer;
+        private System.Windows.Forms.TextBox txtBoxMosquitto;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ AppIOT/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace AppIOT
+{
+    public partial class Form1 : Form
+    {
+        //TODO : Change the base_url to the correct URL
+        private string base_url = "http://localhost:44392/api/somiod/";
+        private string appName;
+        private string containerName;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreateApp_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(base_url);
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
+
+            //Create the object
+            //Send the request to base_url using post and a XML object as the body
+            //Get the response and show it in the textbox
+
+            //XML format
+            /*
+            <application>
+                <name>name</name>
+                <id>**</id>
+                <description>description</description>
+            </application>
+             */
+
+            XmlDocument doc = new XmlDocument();
+            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+
+            XmlNode appNode = doc.CreateElement("application");
+            doc.AppendChild(appNode);
+
+            XmlNode nameNode = doc.CreateElement("name");
+            nameNode.AppendChild(doc.CreateTextNode(txtBoxAppName.Text));
+            appNode.AppendChild(nameNode);
+
+            XmlNode idNode = doc.CreateElement("id");
+            idNode.AppendChild(doc.CreateTextNode("0"));
+            appNode.AppendChild(idNode);
+
+            XmlNode descNode = doc.CreateElement("description");
+            descNode.AppendChild(doc.CreateTextNode("description"));
+            appNode.AppendChild(descNode);
+
+            //Convert the XML to send it via POST
+            StringContent content = new StringContent(doc.OuterXml, Encoding.UTF8, "application/xml");
+
+            //Send the request
+            HttpResponseMessage response = client.PostAsync(base_url,content).Result;
+
+            //req will contain the request and the response
+            string req = response.RequestMessage + Environment.NewLine + response.Content.ReadAsStringAsync().Result;
+
+            //Show the response
+            txtBoxMosquitto.Text = req;
+
+            //if response is OK, extra url is the name of the app and activate the button to create a container
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                appName = txtBoxAppName.Text;
+                btnCreateContainer.Enabled = true;
+            }
+
+        }
+
+        private void btnCreateContainer_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(base_url + appName + "/");
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/xml"));
+
+            //Create the object
+            //Send the request to base_url using post and a XML object as the body
+            //Get the response and show it in the textbox
+
+            //XML format
+            /*
+                <container>
+                    <name>name</name>
+                    <id>**</id>
+                    <description>description</description>
+                </container>
+             */
+
+            XmlDocument doc = new XmlDocument();
+            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+            doc.AppendChild(docNode);
+
+            XmlNode appNode = doc.CreateElement("container");
+            doc.AppendChild(appNode);
+
+            XmlNode nameNode = doc.CreateElement("name");
+            nameNode.AppendChild(doc.CreateTextNode(txtBoxNomeSensor.Text));
+            appNode.AppendChild(nameNode);
+
+            XmlNode idNode = doc.CreateElement("id");
+            idNode.AppendChild(doc.CreateTextNode("0"));
+
+            appNode.AppendChild(idNode);
+
+            XmlNode descNode = doc.CreateElement("description");
+            descNode.AppendChild(doc.CreateTextNode("description"));
+
+            appNode.AppendChild(descNode);
+            doc.AppendChild(docNode);
+
+
+            //Convert the XML to send it via POST
+            StringContent content = new StringContent(doc.OuterXml, Encoding.UTF8, "application/xml");
+
+            //Send the request
+            HttpResponseMessage response = client.PostAsync(base_url + appName + "/", content).Result;
+
+            //req will contain the request and the response
+            string req = response.RequestMessage + Environment.NewLine + response.Content.ReadAsStringAsync().Result;
+
+            //Show the response
+            txtBoxMosquitto.Text = req;
+
+            //if response is OK, extra url is the name of the app/sensor
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                containerName = txtBoxNomeSensor.Text;
+            }
+
+
+        }
+
+        private async void SubscribeToNotifications(string endpoint)
+        {
+            string url = $"{base_url}/{appName}/{containerName}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                XmlDocument doc = new XmlDocument();
+                XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+                doc.AppendChild(docNode);
+
+                XmlNode appNode = doc.CreateElement("notification");
+                doc.AppendChild(appNode);
+
+                XmlNode endpointNode = doc.CreateElement("endpoint");
+                endpointNode.AppendChild(doc.CreateTextNode(endpoint));
+                appNode.AppendChild(endpointNode);
+
+                XmlNode nameNode = doc.CreateElement("name");
+                nameNode.AppendChild(doc.CreateTextNode("notification"));
+                appNode.AppendChild(nameNode);
+
+                string xml = doc.OuterXml;
+
+
+                HttpContent content = new StringContent(xml, Encoding.UTF8, "application/xml");
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Subscribed to notifications!");
+                }
+                else
+                {
+                    MessageBox.Show($"Error: {response.StatusCode}");
+                }
+            }
+        }
+
+
+    }
+
+}
